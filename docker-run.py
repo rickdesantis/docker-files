1ca897c7659414584fbe72f09876da2048cb76d8
Riccardo Benito Desantis <effetti@gmail.com>
2016-05-30 14:12:30 +0200

    Updated the script

--- docker-run.py
+++ docker-run.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python
 
 import argparse
-from subprocess import call
+import subprocess
 
-def run(command):
+def run(command, background=False):
     print command
-    call(command.split())
+    if background:
+        subprocess.Popen(command.split())
+    else:
+        subprocess.call(command.split())
+
 
 def docker_run(args):
     if args == None:
         return
     if args.container == 'firefox':
-        run('docker run --name firefox -p {vnc}:5901 -p {rdp}:3389 -e "GEOMETRY={g}" rickdesantis/firefox'.format(vnc=args.vnc, rdp=args.rdp, g=args.g))
+        run('docker stop firefox')
+        run('docker rm firefox')
+        if not args.stop:
+            run('docker run --name firefox -p {vnc}:5901 -p {rdp}:3389 -e "GEOMETRY={g}" rickdesantis/firefox'.format(vnc=args.vnc, rdp=args.rdp, g=args.g), True)
+            if not args.startonly:
+                run('open vnc://127.0.0.1:5901')
     elif args.container == 'centos':
-        run('docker run --name centos rickdesantis/centos')
+        run('docker stop centos')
+        run('docker rm centos')
+        if not args.stop:
+            run('docker run --name centos rickdesantis/centos', True)
+            if not args.startonly:
+                run('docker exec -it centos bash')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Runs the containers uploaded by the docker hub user rickdesantis.')
@@ -21,5 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("-g", help='the screen resolution that will be used', default='1440x900')
     parser.add_argument("-vnc", help='the port used by VNC', default='5901')
     parser.add_argument("-rdp", help='the port used by RDP', default='3389')
+    parser.add_argument("-stop", help='stops the container', action='store_true')
+    parser.add_argument("-startonly", help='starts the container but don\'t connect to it', action='store_true')
     args = parser.parse_args()
     docker_run(args)
