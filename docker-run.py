02e6a3250bf24fcf94d789be2d82e3247565a092
Riccardo Benito Desantis <effetti@gmail.com>
2016-06-01 17:55:30 +0200

    Bugfixes

--- docker-run.py
+++ docker-run.py
@@ -25,22 +25,18 @@ def docker_run(args):
         run('docker stop {container}'.format(container=args.container))
         run('docker rm {container}'.format(container=args.container))
     if args.operation == 'start':
-        if args.container == 'firefox':
-            run('docker run --name firefox -p {vnc}:5901 -p {rdp}:3389 -e "GEOMETRY={g}" rickdesantis/firefox'.format(vnc=args.vnc, rdp=args.rdp, g=args.g), True)
-        elif args.container == 'centos':
-            run('docker run --name centos rickdesantis/centos')
-    elif args.operation == 'connect' and is_container_running(args.container):
-        if args.container == 'firefox':
-            run('open vnc://127.0.0.1:{vnc}'.format(vnc=args.vnc))
-        elif args.container == 'centos':
-            run('docker exec -it centos bash')
+        run('docker run --name {container} -p {vnc}:5901 -p {rdp}:3389 -e "GEOMETRY={g}" rickdesantis/{container}'.format(container=args.container, vnc=args.vnc, rdp=args.rdp, g=args.g), True)
+    elif args.operation == 'vnc' and is_container_running(args.container):
+        run('open vnc://127.0.0.1:{vnc}'.format(vnc=args.vnc))
+    elif args.operation == 'bash' and is_container_running(args.container):
+        run('docker exec -it {container} bash'.format(container=args.container))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Runs the containers uploaded by the docker hub user rickdesantis.')
-    parser.add_argument("container", help='the name of the container that will be run', choices=['firefox', 'centos'])
+    parser.add_argument("container", help='the name of the container that will be run', choices=['firefox', 'centos', 'lxde'])
     parser.add_argument("-g", help='the screen resolution that will be used', default='1440x900')
     parser.add_argument("-vnc", help='the port used by VNC', default='5901')
     parser.add_argument("-rdp", help='the port used by RDP', default='3389')
-    parser.add_argument("-operation", help='the operation on the container', default='start', choices=['start', 'stop', 'connect'])
+    parser.add_argument("-operation", help='the operation on the container', default='start', choices=['start', 'stop', 'vnc', 'bash'])
     args = parser.parse_args()
     docker_run(args)
