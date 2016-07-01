f55fb1278a8e7a2af536b36605b482c9c8250aa1
Riccardo Benito Desantis <effetti@gmail.com>
2016-07-01 10:12:47 +0200

    Updated the run file and fixed che centos-xfce

--- docker-run.py
+++ docker-run.py
@@ -30,11 +30,13 @@ def docker_run(args):
         run('docker rm {container}'.format(container=args.container))
     if args.operation == 'start':
         run('docker pull rickdesantis/{container}'.format(container=args.container), False)
-        run('docker run --name {container} -p {vnc}:5901 -p {rdp}:3389 -p {http}:80 -e GEOMETRY={g} -e PASSWORD={psw} {folders} rickdesantis/{container}'.format(container=args.container, vnc=args.vnc, rdp=args.rdp, http=args.http, g=args.g, folders=folders, psw=args.psw), True)
+        run('docker run --name {container} -p {vnc}:5901 -p {rdp}:3389 -p {http}:80 -e GEOMETRY={g} -e PASSWORD={psw} {folders} rickdesantis/{container} {cmd}'.format(container=args.container, vnc=args.vnc, rdp=args.rdp, http=args.http, g=args.g, folders=folders, psw=args.psw, cmd=args.cmd), True)
     elif args.operation == 'vnc' and is_container_running(args.container):
         run('open vnc://127.0.0.1:{vnc}'.format(vnc=args.vnc))
     elif args.operation == 'bash' and is_container_running(args.container):
         run('docker exec -it {container} bash'.format(container=args.container))
+    elif args.operation == 'exec' and is_container_running(args.container):
+        run('docker exec -it {container} {cmd}'.format(container=args.container, cmd=args.cmd))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Runs the containers uploaded by the docker hub user rickdesantis.')
@@ -44,7 +46,8 @@ if __name__ == "__main__":
     parser.add_argument("-vnc", help='the port used by VNC', default='5901')
     parser.add_argument("-rdp", help='the port used by RDP', default='3389')
     parser.add_argument("-http", help='the port used by HTTP', default='80')
-    parser.add_argument("-operation", help='the operation on the container', default='start', choices=['start', 'stop', 'vnc', 'bash'])
+    parser.add_argument("-operation", help='the operation on the container', default='start', choices=['start', 'stop', 'vnc', 'bash','exec'])
     parser.add_argument('-mount', nargs='+', help='folders to be mount (<local path>:<container path> separated by spaces')
+    parser.add_argument('-cmd', help='the command to be run', default='')
     args = parser.parse_args()
     docker_run(args)
